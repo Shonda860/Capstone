7ff01fcacb3d2a3b48b110ed9fbc16db204d448a
Shonda Warren <norshonda.warren@gmail.com>
2020-07-10 03:33:48 -0700

    updated nav

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,34 +1,35 @@
 import React, { Component } from 'react';
 // import logo from "./log"
 import {Link} from 'react-router-dom'
+import 'materialize-css/dist/css/materialize.min.css'
 
-
-    const navStyle ={
-      color: 'white'
-    }
 
 class Nav extends Component {
   render () {
-    const style = {
-      margin: 0,
-      top: 0,
-      right: 60,
-   bottom:20,
-      left: 'auto',
-      position: 'fixed',
-  };
+   
 return(
+  <nav>
+  <div class="nav-wrapper">
+    <a href="#" class="brand-logo">Encore</a>
+    <ul id="nav-mobile" class="right hide-on-med-and-down">
+     
+      <li><Link to="/League">Leagues</Link></li>
+      <li><Link to="/Battleraplogy">Battleraplogy</Link></li>
+      <li><Link to="/Signin" >Sign in</Link></li>
+    </ul>
+  </div>
+</nav>
 
-      <nav style={style}>
-        <div className="our-story-header">
-          <ul className="nav-links">
-            <Link style={navStyle} to="/Battlerapogy"><li className="authLinks redButton">Battlerapogy</li></Link>
-            <Link style={navStyle} to="/Leagues"><li className="authLinks redButton">Leagues</li></Link>
-            <Link style={navStyle} to="/Sign in"><li className="authLinks redButton">Sign</li></Link>
-            <Link style={navStyle} to="/SearchBar"><li className="authLinks redButton">Search</li></Link>
-          </ul>
-        </div>
-      </nav>
+      // <nav style={style}>
+      //   <div className="our-story-header">
+      //     <ul className="nav-links">
+      //       <Link style={navStyle} to="/Battlerapogy"><li className="authLinks redButton">Battlerapogy</li></Link>
+      //       <Link style={navStyle} to="/Leagues"><li className="authLinks redButton">Leagues</li></Link>
+      //       <Link style={navStyle} to="/Sign in"><li className="authLinks redButton">Sign</li></Link>
+      //       <Link style={navStyle} to="/SearchBar"><li className="authLinks redButton">Search</li></Link>
+      //     </ul>
+      //   </div>
+      // </nav>
     );
   }
 };
