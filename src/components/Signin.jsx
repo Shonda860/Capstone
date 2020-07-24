0bac7c3c838c06d1da9c095df212e598089b1d6b
Shonda Warren <norshonda.warren@gmail.com>
2020-07-24 03:04:22 -0700

    hopefully last bug of broken demo

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
+
 import GoogleLogin from 'react-google-login'
+
 
 export default (props) => {
   const [name, setName] = useState("");
@@ -12,8 +14,10 @@ export default (props) => {
     props.usernameCallback(response.profileObj.name)
     setEmail(response.profileObj.email)
     setUrl(response.profileObj.imageUrl)
+
     console.log(response)
     console.log(response.profileObj)
+    
   };
 
   
@@ -26,7 +30,8 @@ export default (props) => {
           buttonText="Login"
           onSuccess={responseGoogle}
           onFailure={responseGoogle}
-          cookiePolicy={'single_host_origin'}/>
+          isSignedIn={true}
+          />
       </div>   
       <ul id="slide-out" class="sidenav">
       <li><div class="user-view">
@@ -41,7 +46,7 @@ export default (props) => {
       <li><a href="#!">Vote History</a></li>
       <li><div className="divider"></div></li>
       <li><a href="#!" className="subheader">Favorite Battles</a></li>
-      <li><a href="#1" className="waves-effect">Battle Name#1</a></li>
+      <li><a href="#!" className="waves-effect">Battle Name#1</a></li>
       <li><a  href="#!" className="waves-effect">Battle Name#2</a></li>
     </ul>
     </div>
